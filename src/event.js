876f8a022a88b9cc079f8e9890ea5c2c7ca0014e
Jeremy Chone <jeremy.chone@gmail.com>
2017-05-20 09:35:36 -0700

    #12 mvdom.on - add support for multi event type bindings

--- src/event.js
+++ src/event.js
@@ -16,7 +16,7 @@ module.exports = {
 // - selector: (optional) query selector which will be tested on the target element. 
 // - listener: function which will get the "event" as first parameter
 // - opts: (optional) {ns,ctx} optional namespace and ctx (i.e. this)
-function on(els, type, selector, listener, opts){
+function on(els, types, selector, listener, opts){
 
 	// if the "selector" is a function, then, it is the listener and there is no selector
 	if (selector instanceof Function){
@@ -25,82 +25,89 @@ function on(els, type, selector, listener, opts){
 		opts = listener;
 	}
 
-	var typeSelectorKey = buildTypeSelectorKey(type, selector);
+	types = utils.splitAndTrim(types, ",");
 
-	utils.asArray(els).forEach(function(el){
+	types.forEach(function(type){
+		var typeSelectorKey = buildTypeSelectorKey(type, selector);
 
-		// This will the listener use for the even listener, which might differ
-		// from the listener function passed in case of a selector
-		var _listener = listener; 
+		utils.asArray(els).forEach(function(el){
 
-		// if we have a selector, create the wrapper listener to do the matches on the selector
-		if (selector){
-			_listener = function(evt){
-				var tgt = null;
-				var target = evt.target;
-				var currentTarget = evt.currentTarget;
-				var ctx = (opts)?opts.ctx:null;
-				// if the target match the selector, then, easy, we call the listener
-				if (target.matches(selector)){
-					// Note: While mouseEvent are readonly for its properties, it does allow to add custom properties
-					evt.selectTarget = target;
-					listener.call(ctx,evt);
-				}
-				// now, if it does not, perhaps something in between the target and currentTarget
-				// might match
-				else{
-					tgt = evt.target.parentNode;
-					// TODO: might need to check that tgt is not undefined as well. 
-					while (tgt !== null && tgt !== currentTarget && tgt !== document){
-						if (tgt.matches(selector)){
-							// Note: While mouseEvent are readonly for its properties, it does allow to add custom properties
-							evt.selectTarget = tgt;
-							listener.call(ctx,evt);
-							tgt = null;
-							break;
-						}
+			// This will the listener use for the even listener, which might differ
+			// from the listener function passed in case of a selector
+			var _listener = listener; 
 
-						tgt = tgt.parentNode;
+			// if we have a selector, create the wrapper listener to do the matches on the selector
+			if (selector){
+				_listener = function(evt){
+					var tgt = null;
+					var target = evt.target;
+					var currentTarget = evt.currentTarget;
+					var ctx = (opts)?opts.ctx:null;
+					// if the target match the selector, then, easy, we call the listener
+					if (target.matches(selector)){
+						// Note: While mouseEvent are readonly for its properties, it does allow to add custom properties
+						evt.selectTarget = target;
+						listener.call(ctx,evt);
 					}
-				}
+					// now, if it does not, perhaps something in between the target and currentTarget
+					// might match
+					else{
+						tgt = evt.target.parentNode;
+						// TODO: might need to check that tgt is not undefined as well. 
+						while (tgt !== null && tgt !== currentTarget && tgt !== document){
+							if (tgt.matches(selector)){
+								// Note: While mouseEvent are readonly for its properties, it does allow to add custom properties
+								evt.selectTarget = tgt;
+								listener.call(ctx,evt);
+								tgt = null;
+								break;
+							}
+
+							tgt = tgt.parentNode;
+						}
+					}
+				};
+			}
+			// if we do not have a selector, but still havea  opts.ctx, then, need to wrap
+			else if (opts && opts.ctx){
+				_listener = function(evt){
+					listener.call(opts.ctx,evt);
+				};
+			}
+			
+			var listenerRef = {
+				type: type,
+				listener: listener, // the listener as passed by the user
+				_listener: _listener, // an eventual wrap of the listener, or just point listener.
 			};
-		}
-		// if we do not have a selector, but still havea  opts.ctx, then, need to wrap
-		else if (opts && opts.ctx){
-			_listener = function(evt){
-				listener.call(opts.ctx,evt);
-			};
-		}
-		
-		var listenerRef = {
-			type: type,
-			listener: listener, // the listener as passed by the user
-			_listener: _listener, // an eventual wrap of the listener, or just point listener.
-		};
 
-		if (selector){
-			listenerRef.selector = selector;
-		}
+			if (selector){
+				listenerRef.selector = selector;
+			}
 
-		// If we have a namespace, they add it to the Ref, and to the listenerRefsByNs
-		// TODO: need to add listenerRef in a nsDic if if there a opts.ns
-		if (opts && opts.ns){
-			listenerRef.ns = opts.ns;
-			var listenerRefSetByNs = utils.ensureMap(el,"listenerRefsByNs");
-			var listenerRefSet = utils.ensureSet(listenerRefSetByNs, opts.ns);
-			listenerRefSet.add(listenerRef);
-		}
+			// If we have a namespace, they add it to the Ref, and to the listenerRefsByNs
+			// TODO: need to add listenerRef in a nsDic if if there a opts.ns
+			if (opts && opts.ns){
+				listenerRef.ns = opts.ns;
+				var listenerRefSetByNs = utils.ensureMap(el,"listenerRefsByNs");
+				var listenerRefSet = utils.ensureSet(listenerRefSetByNs, opts.ns);
+				listenerRefSet.add(listenerRef);
+			}
 
-		// add the listenerRef as listener:listenerRef entry for this typeSelectorKey in the listenerDic
-		var listenerDic = utils.ensureMap(el,"listenerDic");
-		var listenerRefByListener = utils.ensureMap(listenerDic,typeSelectorKey);
-		listenerRefByListener.set(listener, listenerRef);
+			// add the listenerRef as listener:listenerRef entry for this typeSelectorKey in the listenerDic
+			var listenerDic = utils.ensureMap(el,"listenerDic");
+			var listenerRefByListener = utils.ensureMap(listenerDic,typeSelectorKey);
+			listenerRefByListener.set(listener, listenerRef);
 
-		// do the binding
-		el.addEventListener(type, _listener);
+			// do the binding
+			el.addEventListener(type, _listener);
 
-		return this;
-	});
+			return this;
+
+		}); // /types.forEach(function(type){
+
+	}); // /types.forEach(function(type){
+
 }
 
 
